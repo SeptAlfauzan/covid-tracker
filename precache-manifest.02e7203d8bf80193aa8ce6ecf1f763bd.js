39f23130dc987e4c1e0c36d848ad58619637f83b
SeptAlfauzan <alfauzansepta@gmail.com>
2020-04-22 11:25:13 +0700

    Updates

---
+++ precache-manifest.02e7203d8bf80193aa8ce6ecf1f763bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8de8cacf21e8f902e54b2894a802610",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "60b2bfd7301ae9bf4e89",
+    "url": "/covid-tracker/static/css/2.5ffc8bb3.chunk.css"
+  },
+  {
+    "revision": "b6ea2421ea8559efb498",
+    "url": "/covid-tracker/static/css/main.58a20946.chunk.css"
+  },
+  {
+    "revision": "60b2bfd7301ae9bf4e89",
+    "url": "/covid-tracker/static/js/2.e72fdddb.chunk.js"
+  },
+  {
+    "revision": "44ca7754162f09506dec3643ba30b719",
+    "url": "/covid-tracker/static/js/2.e72fdddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ea2421ea8559efb498",
+    "url": "/covid-tracker/static/js/main.6ba9e7a0.chunk.js"
+  },
+  {
+    "revision": "5767bd1d46dd2aefcfdb",
+    "url": "/covid-tracker/static/js/runtime-main.63c14202.js"
+  },
+  {
+    "revision": "8fa9e808e1ff57c6859e7a3aebaa2a0a",
+    "url": "/covid-tracker/static/media/Location.8fa9e808.png"
+  }
+]);
